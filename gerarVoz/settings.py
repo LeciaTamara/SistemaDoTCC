d649df7063d9e3df041a573bfbe92b6c89a98c7e
Lécia Tamara <leciatamara60@gmail.com>
2025-10-09 19:25:23 -0300

    adiciono o a pasta geradorDeVoz no staticfiles_dirs para colocar o caminho certo do static para o deploy

--- gerarVoz/settings.py
+++ gerarVoz/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'geradorDeVoz', 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
