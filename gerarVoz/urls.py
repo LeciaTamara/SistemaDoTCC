60e7ced6326157428cfaab6545e776e1d08b8724
Lécia Tamara <leciatamara60@gmail.com>
2025-10-09 21:48:15 -0300

    caminho do estatico ajustado

--- gerarVoz/urls.py
+++ gerarVoz/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nuncaetardeparaaprender/', include('geradorDeVoz.urls')),
+    path('', include('geradorDeVoz.urls')),
 ]
 
 if settings.DEBUG:
